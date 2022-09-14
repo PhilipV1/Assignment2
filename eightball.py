477ced6edcc1b921222a920a5799ba89fae8d6b0
PhilipV1 <philipvelandria@gmail.com>
2022-09-14 14:13:11 +0200

    eightball and list_info

---
+++ eightball.py
@@ -0,0 +1,31 @@
+import random as rd
+
+
+def rand_strings():
+    string_list = [
+        "Well of course",
+        "Absolutely not",
+        "Highly unlikely",
+        "I'm not in the mood ask me later",
+        "What do you think?",
+        "Yes",
+        "No",
+        "That's a weird question"
+    ]
+    return string_list[rd.randint(0, len(string_list) - 1)]
+
+
+def main():
+    continue_loop = True
+
+    while continue_loop:
+        question = input("Ask the magic 8-ball your question: ")
+
+        if question.casefold() == "stop":
+            continue_loop = False
+        else:
+            print(f"The magic 8-ball says: {rand_strings()}")
+
+
+if __name__ == "__main__":
+    main()
