2e20a993324a1110ca6f49d7faa74ebdebfb7e44
PhilipV1 <philipvelandria@gmail.com>
2022-09-16 18:08:16 +0200

    birthday_candles

---
+++ birthday_candles.py
@@ -0,0 +1,26 @@
+# A program that calculates how many boxes of candles that are
+# necessary to purchase for a given birthday. Candles decrement by current age
+def main():
+    candle_box = [0, 0]
+
+    for i in range(1, 101):
+        if i > candle_box[1]:
+            # Taking advantage of floor division rounding down
+            # using the negative number and then negate it back to positive
+            # to effectively round up
+            box_amount = -(-(i - candle_box[1]) // 24)
+            candle_box[0] += box_amount
+            candle_box[1] += (box_amount * 24)
+
+            print(f"Before birthday {i}, buy {box_amount} box(es)")
+
+            candle_box[1] -= i
+        else:
+            candle_box[1] -= i
+
+    print(f"Total number of boxes: {candle_box[0]}, ", end="")
+    print(f"Remaining candles: {candle_box[1]}")
+
+
+if __name__ == "__main__":
+    main()
